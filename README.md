6911513919a0bfb38eed4358e1fdd6688ec6529e
Julien Bouquillon <julien@bouquillon.com>
2015-12-27 22:56:43 +0100

    cleanup

---
+++ README.md
@@ -0,0 +1,14 @@
+# Light breathing server
+
+Make light breath using RaspberryPI GPIO and an HTTP API
+
+## Usage
+
+`python server.py`
+
+Then use API on `http://127.0.0.1:9200` 
+
+## API
+
+ - `/fast` : fast breathing
+ - `/slow` : slow breathing
--- server.py
+++ server.py
@@ -10,23 +10,28 @@ GPIO_FREQUENCY = 120
 
 PORT = 9200
 
+MODES = {
+    'fast': {
+        'intensity': 100,
+        'delay': 0.005
+    },
+    'slow': {
+        'intensity': 30,
+        'delay': 0.05
+    }
+}
+
 if __name__=='__main__':
 
-    thread = Dim(GPIO_LED_PIN, GPIO_FREQUENCY, intensity=30, delay=0.05)
+    thread = Dim(GPIO_LED_PIN, GPIO_FREQUENCY, **MODES['slow'])
     thread.start()
 
     @route('/fast')
     def fast():
-        thread.update({
-            'intensity': 100,
-            'delay': 0.005
-        })
+        thread.update(MODES['fast'])
 
     @route('/slow')
     def slow():
-        thread.update({
-            'intensity': 30,
-            'delay': 0.05
-        })
+        thread.update(MODES['slow'])
 
     run(host='0.0.0.0', port=PORT)
